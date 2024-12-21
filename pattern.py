e529d4d11b77a9b044c269be24855a3cd8bb565c
vikas848 <6391782926vs@gmail.com>
2024-12-21 17:39:46 +0530

    new update

--- pattern.py
+++ pattern.py
@@ -1,11 +1,11 @@
 for a in range(9, 0, -1):  
-    for _ in range(a):     
+    for _ in range(a):                          # int type
         print(a, end=" ")  
     print()                
 
 
 i = str(9)
 for a in range(9, 0, -1):  
-    print(a*i)
+    print(a*i)                               # str type
     i = str(int(i) -1)  
 
