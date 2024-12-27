1abc1446fcd318f006cbd64becf201344bb24ca7
vikas848 <6391782926vs@gmail.com>
2024-12-27 19:31:15 +0530

    new update

--- Maths1.py
+++ Maths1.py
@@ -1,9 +1,17 @@
+# b = []
+# for a in range(0, 6, 1):
+#         a=int(input())
+#         b.append(a)
+ 
+# print(b[1::2])
+# print(b[0::2])
+
+
 b = []
 for a in range(0, 6, 1):
         a=int(input())
-        b.append(a)
- 
-print(b[1::2])
-print(b[0::2])
-
-
+        if a%2==0:
+                print(a)
+        else:
+                b.append(a)
+print(b)
